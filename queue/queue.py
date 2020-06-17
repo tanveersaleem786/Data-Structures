a2cc4c6387c097fc6b46f2d8e2013fd847fa8581
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-06-17 19:52:18 -0400

    Queue implement using list

--- queue/queue.py
+++ queue/queue.py
@@ -16,13 +16,50 @@ Stretch: What if you could only use instances of your Stack class to implement t
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
+        # self.header = None
+        # self.tail = None
     
     def __len__(self):
-        pass
+        return self.size
 
-    def enqueue(self, value):
-        pass
+    def enqueue(self, value):  
+        #self.storage.insert(0,value)
+        self.storage.append(value)
+        self.size += 1
+        # if self.header is None and self.tail is None:    
+            
+        #     self.size += 1
+        #     self.header = 0
+        #     self.tail = 0    
+        # else:
+        #     self.storage.append(value)
+        #     self.size += 1            
+        #     self.tail += 1    
+
+    
 
     def dequeue(self):
-        pass
+        if self.size != 0:
+            self.size -= 1
+            #return self.storage.pop()
+            return self.storage.pop(0)
+        else:
+            return None
+        # if self.size != 0:
+        #     self.storage.remove(self.storage[self.header])
+        #     self.size -= 1
+        #     self.tail -= 1
+        
+        
+#qu = Queue()
+#https://runestone.academy/runestone/books/published/pythonds/BasicDS/ImplementingaQueueinPython.html
+# qu = Queue()
+# print(qu.__len__())
+# qu.enqueue(2)
+# print(qu.__len__())
+# qu.enqueue(3)
+# print(qu.__len__())
+
+# qu.dequeue()
+# print(qu.__len__())
